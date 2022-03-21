7b76f69889d59f7e8c3cceb99a8a4fecbfdd17c5
Eeva <eeva.hanka@iki.fi>
2022-03-21 19:57:13 +0200

    fixed ui

--- src/ui.py
+++ src/ui.py
@@ -1,10 +1,20 @@
 import io
 
-
 class Ui:
     def __init__(self, io) -> None:
         self.io = io
 
     def start(self):
         self.io.output("welcome to compressing")
-        
+        file_to_compress = self.get_file()
+        self.compressing(file_to_compress)
+
+    def get_file(self):
+        #this is temporary 
+        file_to_compress = self.io.input("what do you want to compress? ")
+        return file_to_compress
+
+    def compressing(self, file_to_compress):
+        self.io.output("compressing...")
+        #will call compressing algorthims !
+        self.io.output("this feature doesn't exist!")
