017d52148b149a5710bb0a66a7f001c4cd719bc8
Eeva <eeva.hanka@iki.fi>
2022-04-22 18:14:10 +0300

    fixed file_acces

--- src/file_acces.py
+++ src/file_acces.py
@@ -1,6 +1,5 @@
 import os
 
-
 def open_file(file_path):
     file = open(file_path, "r")
     content = file.read()
@@ -17,15 +16,16 @@ def get_file_size(file_path):
     return file_size.st_size
 
 def read_bytes(file_path):
-    #no working apparently
-    file = open(file_path, "rb")
-    content = file.read()
-    print(content)
-    bina = ''.join(format(ord(byte), '08b') for byte in str(content)[1:])
+    with open(file_path, "rb") as file:
+        content = file.read()
+    file.close()
+    hexa = content.hex()
+    bina = str("{0:08b}".format(int(hexa, 16)))
+    os.remove(file_path)
     return bina
 
 def write_bytes(file_path, content):
-    bytes_to_write  = bytes(int(content[i : i +8], 2) for i in range(0, len(content), 8))
-    bin_file = open(file_path, "wb")
-    bin_file.write(bytes_to_write)
+    bytes_to_write = bytes(int(content[i : i +8], 2) for i in range(0, len(content), 8))
+    with open(file_path, "wb") as bin_file:
+        bin_file.write(bytes_to_write)
     bin_file.close()
